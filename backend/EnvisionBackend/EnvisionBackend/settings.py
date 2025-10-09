6a939c027807afcb3df8a382ddd8c7ed01f533ed
tauqeerahson07 <97336093+tauqeerahson07@users.noreply.github.com>
2025-10-09 11:16:21 +0500

    deployment--issues

--- backend/EnvisionBackend/EnvisionBackend/settings.py
+++ backend/EnvisionBackend/EnvisionBackend/settings.py
@@ -164,5 +164,5 @@ REST_FRAMEWORK = {
 }
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=365)
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
 }
